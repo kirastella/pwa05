b20ed8b9b616bf780e5a2842334fa46911769ed0
kirastella <72187326+kirastella@users.noreply.github.com>
2022-01-12 11:36:08 +0100

    initial commmit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import OneSignal from 'react-onesignal';
 
 function App() {
+  
+  useEffect(() => {
+    OneSignal.init({
+      appId: "83ef08d6-6c35-46ac-bf96-961cb844e95d",
+    });
+  }, []);
+
+
+
   return (
     <div className="App">
       <header className="App-header">
